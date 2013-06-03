47fb16425033ee623f3d3badfd82ed3bcf72e228
omniaura <omniaura5@gmail.com>
2013-06-02 22:30:58 -0700

    jquery expanding article list

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
-    $('#article-list div').hide();
-    $('#article-list div').click(function() {
-        ;
+    //hide the article list when the DOM is ready
+    $('#article-list ul').hide();
+    
+    //roll up the article list when anywhere is clicked
+    $('html').click(function(){
+        $('#article-list ul').slideUp();
     })
+    
+    $('nav a').click(function(event) {
+        event.stopPropagation(); //override the click anywhere roll up
+        var id = $(this).attr('id');
+        
+        if (!$('#article-list #'+id).is(':visible')) { //skip if the section clicked is already visible
+            $('#article-list ul').slideUp();
+            console.log(id);
+            $('#article-list #'+id).slideToggle();            
+        }
+        return false;
+    });
 })
