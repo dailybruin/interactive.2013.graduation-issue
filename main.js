d95c3f0ab1e01b410b062067b3d22620b2ae62f4
omniaura <omniaura5@gmail.com>
2013-06-03 01:06:19 -0700

    improved article-list behavior

--- main.js
+++ main.js
@@ -5,16 +5,16 @@ $(document).ready(function(){
     //roll up the article list when anywhere is clicked
     $('html').click(function(){
         $('#article-list ul').slideUp();
-    })
+    });
     
     $('nav a').click(function(event) {
         event.stopPropagation(); //override the click anywhere roll up
         var id = $(this).attr('id');
         
-        if (!$('#article-list #'+id).is(':visible')) { //skip if the section clicked is already visible
-            $('#article-list ul').slideUp();
-            console.log(id);
-            $('#article-list #'+id).slideToggle();            
+        //roll up all uls
+        $('#article-list ul').slideUp();
+        if (!$('#article-list #'+id).is(':visible')) { //skip if the section clicked is already visible (prevents a "bounce")
+            $('#article-list #'+id).slideToggle();
         }
         return false;
     });
