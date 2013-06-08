8700229ddc32fd1da955b3ecfc9a01fe17219b47
omniaura <omniaura5@gmail.com>
2013-06-07 22:49:54 -0700

    removed article list dropdown

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
 $(document).ready(function(){
-    //hide the article list when the DOM is ready
-    $('#article-list ul').hide();
-    
-    //roll up the article list when anywhere is clicked
-    $('html').click(function(){
-        $('#article-list ul').slideUp();
-    });
-    
-    $('nav a').click(function(event) {
-        event.stopPropagation(); //override the click anywhere roll up
-        var id = $(this).attr('id');
-        
-        //roll up all uls
-        $('#article-list ul').slideUp();
-        if (!$('#article-list #'+id).is(':visible')) { //skip if the section clicked is already visible (prevents a "bounce")
-            $('#article-list #'+id).slideDown();
-            //$('#'+id+':before').css({'margin-right': '10px'}); not working
-        }
-        return false;
-    });
+
 })
